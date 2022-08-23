d67eac4792a9010ca030d51e2dc39d39845cef66
Tommy Karlsson <tommy.karlsson@fortnox.se>
2022-08-23 11:28:56 +0200

    Remove unnecessary logging
    Added go module

--- fnxlogrus.go
+++ fnxlogrus.go
@@ -6,7 +6,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-//Init initializes logrus with level and formatter
+// Init initializes logrus with level and formatter
 func Init(config Config, l *logrus.Logger) {
 	if config.Level == "" {
 		config.Level = "info"
@@ -17,10 +17,8 @@ func Init(config Config, l *logrus.Logger) {
 		log.Fatalln(err)
 	}
 
-	log.Printf("fnxlogrus: setting logrus logging level to %s\n", lvl)
 	l.SetLevel(lvl)
 	if config.formatter() != nil {
-		log.Printf("fnxlogrus: setting logrus logging formatter to %T\n", config.formatter())
 		l.SetFormatter(config.formatter())
 	}
 }
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/fortnoxab/fnxlogrus
+
+go 1.19
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
